1a1cc67d8334b0932db9dd4e967cfb3fee1cbf31
Eleni Mijalis <elenimijalis@gmail.com>
2016-05-23 22:29:24 -0500

    working

---
+++ script.py
@@ -0,0 +1,51 @@
+import argparse
+import json
+import urllib
+
+def print_times(vids):
+    ''' prints individual video times and total playlist time '''
+
+    total_time= 0
+    for vid in vids:
+        total_time += vids[vid]
+
+        print vid
+        m, s = divmod(vids[vid], 60)
+        h, m = divmod(m, 60)
+        print '%d:%02d:%02d' % (h, m, s)
+
+    m, s = divmod(total_time, 60)
+    h, m = divmod(m, 60)
+    print '****************'
+    print 'TOTAL TIME:'
+    print '%d:%02d:%02d' % (h, m, s)
+    print '****************'
+
+def get_video_times(url):
+    ''' returns dictionary of video names and lengths '''
+
+    response = urllib.urlopen("http://www.khanacademy.org/api/v1/topic/" + url)
+    data = json.loads(response.read())
+    title = data['standalone_title']
+    vids = {}
+
+    for child in data['children']:
+        seconds = 0
+        subtopic = urllib.urlopen("http://www.khanacademy.org/api/v1/topic/" + child['id'] + '/videos')
+        videos = json.loads(subtopic.read())
+
+        for video in videos:
+            seconds += video['duration']
+        vids[child['translated_title']] = seconds
+    return vids, title
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Khan Playlist Durations')
+    parser.add_argument('url', help='Enter url of playlist')
+    args = parser.parse_args()
+
+    vid_times, title = get_video_times(args.url.rsplit('/', 1)[-1])
+    print '//////////////////////////////'
+    print 'Playlist: %s' % title
+    print '//////////////////////////////'
+    print_times(vid_times)
