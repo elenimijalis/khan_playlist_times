02a6d19824dfb957add43c4406d5d0253c94c2d2
Eleni Mijalis <elenimijalis@gmailcom>
2017-10-30 22:19:12 -0500

    added 2x speed report

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # KhanAcademy Playlist Length Calculator
-Just paste the url while invoking the script.
+Have you ever wondered how long it will take to watch all the videos in a category?
+Just paste the url of the category in question while invoking the script.
 ```console
 $ python script.py https://www.khanacademy.org/science/health-and-medicine/human-anatomy-and-physiology
 ```
+
+The output will give you the length for each subcategory and the final total amount of time it will take to view all the videos in the category!
--- script.py
+++ script.py
@@ -2,10 +2,11 @@ import argparse
 import json
 import urllib
 
+
 def print_times(vids):
     ''' prints individual video times and total playlist time '''
 
-    total_time= 0
+    total_time = 0
     for vid in vids:
         total_time += vids[vid]
 
@@ -16,10 +17,17 @@ def print_times(vids):
 
     m, s = divmod(total_time, 60)
     h, m = divmod(m, 60)
+
+    # at 2x speed
+    fast_m, fast_s = divmod(total_time/2, 60)
+    fast_h, fast_m = divmod(fast_m, 60)
+
     print '****************'
     print 'TOTAL TIME:'
     print '%d:%02d:%02d' % (h, m, s)
     print '****************'
+    print 'Note: if you watch at 2x speed, it will only take %d:%02d:%02d!' % (fast_h, fast_m, fast_s)
+
 
 def get_video_times(url):
     ''' returns dictionary of video names and lengths '''
@@ -38,6 +46,7 @@ def get_video_times(url):
             seconds += video['duration']
         vids[child['translated_title']] = seconds
     return vids, title
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Khan Playlist Durations')
